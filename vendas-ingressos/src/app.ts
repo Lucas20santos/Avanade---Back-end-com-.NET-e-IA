0e949de30ac462658cbf10128014810fa2ae7121
lucas de souza santos <lucass.santoss210@gmail.com>
2025-01-24 17:57:09 +0000

    modificações

--- vendas-ingressos/src/app.ts
+++ vendas-ingressos/src/app.ts
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
     res.json({message: "Hello World"});
 });
 
+app.post("/auth/login", (req, res) => {
+    const {name, password} = req.body;
+    console.log(`name = ${name}, password = ${password}`);
+    res.send();
+});
+
 app.listen(3000, () => {
     console.log("running in http://localhost:3000");
 });
