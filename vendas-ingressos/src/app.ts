a63f75ea6c30a0da5cef95667710bf4b3d254f78
lucas de souza santos <lucass.santoss210@gmail.com>
2025-01-24 18:39:32 +0000

    criação das rotas

--- vendas-ingressos/src/app.ts
+++ vendas-ingressos/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 const app = express()
 
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.json({message: "Hello World"});
@@ -9,7 +10,47 @@ app.get("/", (req, res) => {
 
 app.post("/auth/login", (req, res) => {
     const {email, password} = req.body;
-    console.log(`name = ${email}, password = ${password}`);
+    console.log(`email = ${email}, password = ${password}`);
+    res.send();
+});
+
+app.post("/partners", (req, res) => {
+    const { name, email, password, company_name } = req.body;
+    res.send();
+});
+
+app.post("/partners/events", (req, res) => {
+    const { name, description, date, location } = req.body;
+    res.send();
+});
+
+app.get("/partners/events", (req, res) => {
+    console.log();
+})
+
+app.get("/partners/events:eventsId", (req, res) => {
+    const { eventsId } = req.params;
+    console.log(eventsId);
+    res.send();
+});
+
+app.post("/customers", (req, res) => {
+    const { name, email, password, address, phone_number} = req.body;
+    res.send();
+});
+
+app.post("/events", (req, res) => {
+    const { name, description, date, location } = req.body;
+    res.send();
+});
+
+app.get("/events", (req, res) => {
+    console.log();
+})
+
+app.get("/events:eventsId", (req, res) => {
+    const { eventsId } = req.params;
+    console.log(eventsId);
     res.send();
 });
 
