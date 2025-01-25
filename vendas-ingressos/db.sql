4342ff88e2b17379598c85f6e43c572ee92dc1fc
lucas de souza santos <lucass.santoss210@gmail.com>
2025-01-25 14:02:07 +0000

    ultimas mudanças do anterior

--- vendas-ingressos/db.sql
+++ vendas-ingressos/db.sql
@@ -1,44 +1,44 @@
-use tickets;
+USE tickets;
 
-CREATE TABEL IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP NOT NULL,
-    PRIMARY KEY (`id`))
-ENGINE = InnoDB
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
-CREATE TABEL IF NOT EXISTS `partners` (
+CREATE TABLE IF NOT EXISTS `partners` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `company_name` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP NOT NULL,
     `user_id` INT NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `fk_partners_users1` (`user_id` ASC) VISIBLE,
+    INDEX `fk_partners_users1` (`user_id` ASC),
     CONSTRAINT `fk_partners_users1`
         FOREIGN KEY (`user_id`)
-        REFERENCES `mydb`.`users` (`id`)
+        REFERENCES `users` (`id`)
         ON DELETE NO ACTION
-        ON UPDATE NO ACTION) 
-ENGINE = InnoDB;
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
-CREATE TABEL IF NOT EXISTS `custmers` (
+CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `address` VARCHAR(255) NOT NULL,
     `phone` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP NOT NULL,
     `user_id` INT NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `fk_customers_users` (`user_id` ASC) VISIBLE,
+    INDEX `fk_customers_users` (`user_id` ASC),
     CONSTRAINT `fk_customers_users`
         FOREIGN KEY (`user_id`)
-        REFERENCES `mydb`.`users` (`id`)
+        REFERENCES `users` (`id`)
         ON DELETE NO ACTION
-        ON UPDATE NO ACTION) 
-ENGINE = InnoDB;
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
-CREATE TABEL IF NOT EXISTS `events` (
+CREATE TABLE IF NOT EXISTS `events` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `description` VARCHAR(255) NOT NULL,
@@ -47,10 +47,10 @@ CREATE TABEL IF NOT EXISTS `events` (
     `created_at` TIMESTAMP NOT NULL,
     `partner_id` INT NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `fk_events_partners1_idx` (`user_id` ASC) VISIBLE,
-    CONSTRAINT `fk_events_partners1_idx`
+    INDEX `fk_events_partners1_idx` (`partner_id` ASC),
+    CONSTRAINT `fk_events_partners1`
         FOREIGN KEY (`partner_id`)
-        REFERENCES `mydb`.`partners` (`id`)
+        REFERENCES `partners` (`id`)
         ON DELETE NO ACTION
-        ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+        ON UPDATE NO ACTION
+) ENGINE = InnoDB;
