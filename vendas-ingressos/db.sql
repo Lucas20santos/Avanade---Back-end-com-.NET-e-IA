3a8c63b3ce73d48ae6a88382bcf26e8328d1ff34
lucas de souza santos <lucass.santoss210@gmail.com>
2025-01-27 23:41:45 +0000

    modificando as alterações

--- vendas-ingressos/db.sql
+++ vendas-ingressos/db.sql
@@ -3,7 +3,7 @@ USE tickets;
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP NOT NULL,
     PRIMARY KEY (`id`)
