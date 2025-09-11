a40026653d304a8a8d32324f3145fe52956a917b
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-09-11 14:24:05 +0000

    modificações no primeiro codigo.

--- codigos/obj/Debug/net8.0/codigos.AssemblyInfo.cs
+++ codigos/obj/Debug/net8.0/codigos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("codigos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03dd959f3987de2b15791b95fb5e21c3e1144a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63987cca34c214efc077936c19e2327fd7bf3a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("codigos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("codigos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
