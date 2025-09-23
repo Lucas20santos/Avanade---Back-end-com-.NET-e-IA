74a2ae35041ee0f213afcdc1dc1a60d98ca65f9a
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-09-23 22:10:17 +0000

    novo codigos.

---
+++ Fundamentos/principal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fundamentos.principal
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Aló, mundo.");
+        }
+    }
+}
