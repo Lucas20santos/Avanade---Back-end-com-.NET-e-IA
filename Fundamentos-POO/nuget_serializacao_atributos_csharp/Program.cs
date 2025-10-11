d250e2b7fb55a8c8eab53d7fd7a43909468f4a1a
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-10-11 00:03:21 +0000

    serializando objetos

--- Fundamentos-POO/nuget_serializacao_atributos_csharp/Program.cs
+++ Fundamentos-POO/nuget_serializacao_atributos_csharp/Program.cs
@@ -1,4 +1,23 @@
 ﻿using nuget_serializacao_atributo_csharp.Models;
+using Newtonsoft.Json;
+
+List<Pessoa> listaPessoa = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa();
+p1.Nome = "Lucas";
+p1.Sobrenome = "Santos";
+p1.idade = 33;
+p1.apresentar();
 
+Pessoa p2 = new()
+{
+    Nome = "Maria",
+    Sobrenome = "Santos",
+    idade = 34
+};
+
+listaPessoa.Add(p1);
+listaPessoa.Add(p2);
+
+string serializacao = JsonConvert.SerializeObject(listaPessoa, Formatting.Indented);
+File.WriteAllText("lista_pessoa.json", serializacao);
