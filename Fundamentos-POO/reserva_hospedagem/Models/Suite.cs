3df6ed00a62b5851de559df0ebd3b82bceeac563
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-10-13 00:37:24 +0000

    terminado validações na classe suite.

--- Fundamentos-POO/reserva_hospedagem/Models/Suite.cs
+++ Fundamentos-POO/reserva_hospedagem/Models/Suite.cs
@@ -8,9 +8,9 @@ namespace reserva_hospedagem.Models
         public Suite() { }
         public Suite(TipoSuite tipoSuite, int capacidade, decimal valor_diario)
         {
-            this._tipoSuite = tipoSuite;
-            this._capacidade = capacidade;
-            this._valorDiario = valor_diario;
+            TipoSuite = tipoSuite;
+            Capacidade = capacidade;
+            ValorDiario = valor_diario;
         }
         // Fazendo validação do Enum para caso o valor vier de uma fonte externa
         public TipoSuite TipoSuite
@@ -18,10 +18,7 @@ namespace reserva_hospedagem.Models
             get => _tipoSuite;
             set
             {
-                if (!Enum.IsDefined(typeof(TipoSuite), value))
-                {
-                    throw new ArgumentException("Tipo de suíte invlálido.");
-                }
+                if (!Enum.IsDefined(typeof(TipoSuite), value)) throw new ArgumentException("Tipo de suíte invlálido.");
                 _tipoSuite = value;
             }
         }
@@ -30,10 +27,7 @@ namespace reserva_hospedagem.Models
             get => _capacidade;
             set
             {
-                if(value < 1)
-                {
-                    throw new ArgumentException("Valor informado não está correto, por favor corrija.");
-                }
+                if(value < 1) throw new ArgumentException("Valor informado não está correto, por favor corrija.");
                 _capacidade = value;
             }
         }
@@ -42,10 +36,7 @@ namespace reserva_hospedagem.Models
             get => _valorDiario;
             set
             {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Valor negativo para o preço da Suite não é aceito. Por favor, corrija.");
-                }
+                if (value <= 0) throw new ArgumentException("Valor negativo para o preço da Suite não é aceito. Por favor, corrija.");
                 _valorDiario = value;
             }
         }
