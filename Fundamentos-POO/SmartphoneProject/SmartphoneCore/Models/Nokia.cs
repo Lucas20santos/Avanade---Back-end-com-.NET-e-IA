fe3e8e3bbfcca7c318e51b1a394104910e187f13
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-10-18 01:52:45 +0000

    Ajustando o codigo do Nokia.

--- Fundamentos-POO/SmartphoneProject/SmartphoneCore/Models/Nokia.cs
+++ Fundamentos-POO/SmartphoneProject/SmartphoneCore/Models/Nokia.cs
@@ -2,12 +2,7 @@ namespace SmartphoneCore.Models
 {
     public class Nokia : Smartphore
     {
-        public Nokia(string numero, string modelo, string imei, int memoria) 
-               : base(numero, modelo, imei, memoria)
-        {
-
-        }
-        
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
         public override Task InstalarAplicativoAsync(App app)
         {
             Console.WriteLine("Instlaando o aplicativos {app.Nome} no Nokia");
