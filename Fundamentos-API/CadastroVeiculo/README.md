45caa71316a3e5f83b184db0904dff450a447df8
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-10-23 23:58:43 +0000

    criação da rota login para verificar a autenticação do usuario.

--- Fundamentos-API/CadastroVeiculo/README.md
+++ Fundamentos-API/CadastroVeiculo/README.md
@@ -2,6 +2,15 @@
 
 ## comando para criação do projeto
 
-```dotnet
+```bash
     dotnet new web -o minimal-api
+```
+
+## Rodando a aplicação
+
+```bash
+    cd minimal-api
+    dotnet run
+    # ou voce pode rador também
+    dotnet watch run # toda vez que você fizer uma modificação no projeto, vc não precisa startar novamente para subir as alterações.
 ```
--- Fundamentos-API/CadastroVeiculo/minimal-api/Program.cs
+++ Fundamentos-API/CadastroVeiculo/minimal-api/Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-app.Run();
+        app.MapGet("/", () => "Hello World!");
+
+        // Criando uma rota para validar o login
+        app.MapPost("/login", (LoginDTO loginDTO) =>
+        {
+            if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+            {
+                return Results.Ok("Login com sucesso");
+            }
+            else
+            {
+                return Results.Unauthorized();
+            }
+        });
+
+
+        app.Run();
+    }
+}
+
+public class LoginDTO
+{
+    public string? Email { get; set; }
+    public string? Senha { get; set; }    
+}
