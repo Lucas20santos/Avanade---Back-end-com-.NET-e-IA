4d4f641e4dc1d7285eadf1be75c0d2fec076583e
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-10-27 23:53:09 +0000

    subindo outras mudanças encontradas.

--- Fundamentos-API/FundamentosApi/obj/Debug/net8.0/FundamentosApi.AssemblyInfo.cs
+++ Fundamentos-API/FundamentosApi/obj/Debug/net8.0/FundamentosApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FundamentosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c141f0b40f6bf31e3efc0b36f4e72be8f528fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa30146de40641ba9ce241492e6ab383015305b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FundamentosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FundamentosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
