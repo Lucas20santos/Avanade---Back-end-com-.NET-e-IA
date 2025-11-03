50ef73eb7eaf4583e76af5fd3b9547090b6f0f64
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-11-03 23:00:34 +0000

    Instalação do dotnet entity framework e do Microsoft.EntityFrameworkCore.Design

--- Fundamentos-API/FundamentosWebApi/obj/Debug/net8.0/FundamentosWebApi.AssemblyInfo.cs
+++ Fundamentos-API/FundamentosWebApi/obj/Debug/net8.0/FundamentosWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FundamentosWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa30146de40641ba9ce241492e6ab383015305b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253d6f219f872c71fb285ef41a1d541cdf24a568")]
 [assembly: System.Reflection.AssemblyProductAttribute("FundamentosWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FundamentosWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
