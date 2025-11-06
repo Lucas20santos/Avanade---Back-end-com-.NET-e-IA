2314d4e3f81d309cf32f86a9e9376df6b07090a4
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-11-06 15:08:02 +0000

    Subindo novas mudanças

--- Fundamentos-API/ApiTarefas/ApiTarefaMVC/Controllers/TarefasController.cs
+++ Fundamentos-API/ApiTarefas/ApiTarefaMVC/Controllers/TarefasController.cs
@@ -1,12 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ApiTarefaMVC.Context;
+using ApiTarefaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTarefaMVC.Controllers
 {
-    public class TarefasController
+    public class TarefasController : Controller
     {
-        
+        private readonly OrganizadorContext _context;
+
+        public TarefasController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var tarefas = _context.Tarefas.ToList();
+
+            return View(tarefas);
+        }
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Criar(Tarefa tarefa)
+        {
+            _context.Add(tarefa);
+            _context.SaveChanges();
+            return Created();
+        }
     }
 }
