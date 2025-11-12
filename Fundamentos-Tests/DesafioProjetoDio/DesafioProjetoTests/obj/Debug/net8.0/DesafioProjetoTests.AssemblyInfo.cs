09631cde054c2ac51431925dc1166020c36062e9
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-11-12 23:31:18 +0000

    Criação da classe teste de validação da lsita...

--- Fundamentos-Tests/DesafioProjetoDio/DesafioProjetoTests/obj/Debug/net8.0/DesafioProjetoTests.AssemblyInfo.cs
+++ Fundamentos-Tests/DesafioProjetoDio/DesafioProjetoTests/obj/Debug/net8.0/DesafioProjetoTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioProjetoTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c9272b4888ed90058694f2c0fbb5fd0f1814307")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebbf1671271fd92c37a9040dea4495c6da3ae11d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioProjetoTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioProjetoTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
