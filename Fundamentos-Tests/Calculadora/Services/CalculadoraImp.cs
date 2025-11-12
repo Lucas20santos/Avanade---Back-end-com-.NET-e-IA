2b7ca0b38531201c791fd75a478ecd5e45f2c232
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-11-12 01:13:22 +0000

    Subindo criação de classe CalculadoraImp

---
+++ Fundamentos-Tests/Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,10 @@
+namespace Calculadora.Services
+{
+    public class CalculadoraImp
+    {
+        public int Somar(int num1, int num2) => num1 + num2;
+        public int Subtrair(int num1, int num2) => num1 - num2;
+        public int Multiplicar(int num1, int num2) => num1 * num2;
+        public float Dividir(int num1, int num2) => num2 == 0 ? 0: num1 / num2;
+    }
+}
--- Fundamentos-Tests/CalculadoraTests/obj/Debug/net8.0/CalculadoraTests.AssemblyInfo.cs
+++ Fundamentos-Tests/CalculadoraTests/obj/Debug/net8.0/CalculadoraTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6da9e885ac3f90af1414e74718c880b593d6b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9732a7ac2e2f8919e5b4c2c14a154cf87011d38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
