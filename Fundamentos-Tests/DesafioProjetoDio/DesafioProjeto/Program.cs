ceceaa2c9d9891721026b43c695a84aae65691e6
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-11-12 21:13:30 +0000

    criação de codigo para verificar as classes.

--- Fundamentos-Tests/DesafioProjetoDio/DesafioProjeto/Program.cs
+++ Fundamentos-Tests/DesafioProjetoDio/DesafioProjeto/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesafioProjeto.Services;
+
+ValidacoesLista vl = new ValidacoesLista();
+
+// Achar numero dentro da lista
+Console.WriteLine(vl.ListaContemDeterminadoNumero([1, 2, 3, 4, 5], 4));
+
+
+// Retornar lista sem numero negativo
+foreach (var item in vl.RemoverNumerosNegativos([-1, 2, 3, -5]))
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
