036400fe6b7243a8871bcd3d7b31862a03a02ccf
Lucas de Souza Santos <lucass.santoss210@gmail.com>
2025-11-18 16:32:34 -0300

    Resolução do primeiro exercicio

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ################### Exercicio 01 ###################
+// 1. "Escreva um programa que imprima ""Hello, C# World!"" no console.","I/O, Console.WriteLine()"
+
+Console.WriteLine("Hello, c# World!");
